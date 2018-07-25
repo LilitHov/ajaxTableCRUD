40c2a0b7b3b141b8504e8de727baf6c4a0df3f91
Lilit Hovakimyan <hlilit19@gmail.com>
2018-07-25 21:01:34 +0400

    registration and log in

--- js/script.js
+++ js/script.js
@@ -5,32 +5,65 @@ $(document).ready(function () {
         $(this).tab('show')
     });
 
-    // ajax task 1
-        var url = 'http://ajaxtesting.somee.com/api/values/';
-        var tableBody = $('.user-info tbody');
 
+    var url = 'http://ajaxtesting.somee.com/api/values/';
+    var tableBody = $('.user-info tbody');
+
+    function getUsers() {
 
         $.ajax({
             url: url+'getusers',
             type:'GET',
             success: function (response) {
                 response.forEach(function (index,i) {
-                        var $tr = $('<tr>').append(
-                            $('<td>').text(i+1),
-                            $('<td>').text(index.Id),
-                            $('<td>').text(index.username),
-                            $('<td>').text(index.password)
-                        );
-                        $tr.appendTo(tableBody);
-                })
+                    var tr = $('<tr>').append(
+                        $('<td>').text(i+1),
+                        $('<td>').text(index.Id),
+                        $('<td>').text(index.username),
+                        $('<td>').text(index.password)
+                    );
+                    tr.appendTo(tableBody);
+                });
             }
+        });
+    }
+    getUsers();
+    function sendAjax(type,username,password) {
+        $.ajax({
+            url: url + type,
+            type: 'POST',
+            data: {
+                username:username,
+                password:password },
+            success: function (response) {
+                if (type === 'login') {
+                    if (response) {
+                        alert('ok')
+                    } else {
+                        alert('error')
+                    }
+                } else if (type === 'registration') {
+                    tableBody.html('');
+                    alert();
+                    getUsers();
+                }
+            }
+        });
+    }
 
-
-
-
+    // console.log(userDetails);
+$('.login').on('click',function () {
+    var username = $('input[name="username"]').val();
+    var password = $('input[name="password"]').val();
+    // console.log(username,password);
+        sendAjax('login',username,password);
+});
+    $('.registration').on('click',function () {
+        var username = $('input[name="regUsername"]').val();
+        var password = $('input[name="regPassword"]').val();
+        // console.log(username,password);
+        sendAjax('registration',username,password);
     });
-
-
 
 
 });
